8e8b62a13a8c8830bde1d756db2f67b31060f111
cetRide <okolacetric@gmail.com>
2019-12-18 19:36:56 +0300

    web pack

---
+++ dist/precache-manifest.d516ac00848f925e1c3311045d6ce181.js
@@ -0,0 +1,6 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c0f7d69c987a50b356",
+    "url": "main.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log("Hello World gvgfgf");
+function getStudent() {
+    var id = document.getElementById("id").value;
+    var name = document.getElementById("name").value;
+    document.writeln(id +"" + name);
+}
+console.log(getStudent());
